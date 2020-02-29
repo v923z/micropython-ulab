380b8b034778180848a2c6dcf987fdc90c79c627
Jeff Epler <jepler@unpythonic.net>
2020-02-29 17:00:53 -0600

    Add new test of slicing
    Closes: #32

---
+++ tests/slicing.py
@@ -0,0 +1,21 @@
+try:
+    import ulab as np
+except:
+    import numpy as np
+
+for num in range(1,4):
+    l = list(range(num))
+    a = np.array(l, dtype=np.int8)
+    for start in range(-num, num+1):
+        for end in range(-num, num+1):
+            for stride in (-3, -2, -1, 1, 2, 3):
+                sl = l[start:end:stride]
+                ll = len(sl)
+                try:
+                    sa = list(a[start:end:stride])
+                    la = len(sa)
+                except IndexError as e:
+                    sa = str(e)
+                    la = -1
+                print("%2d [% d:% d:% d]     %-24r %-24r%s" % (
+                    num, start, end, stride, sl, sa, " ***" if sa != sl else ""))
