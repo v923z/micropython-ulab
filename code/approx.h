bdaa4b3984a72df2731b0257bfcea2dc95d14b12
Zoltán Vörös <zvoros@gmail.com>
2020-07-16 22:05:33 +0200

    fixed linting issues

--- code/approx.h
+++ code/approx.h
@@ -23,13 +23,13 @@ typedef struct _mp_obj_float_t {
     mp_float_t value;
 } mp_obj_float_t;
 
-#define 	APPROX_EPS			1.0e-4
-#define 	APPROX_NONZDELTA	0.05
-#define 	APPROX_ZDELTA		0.00025
-#define		APPROX_ALPHA		1.0
-#define		APPROX_BETA			2.0
-#define		APPROX_GAMMA		0.5
-#define		APPROX_DELTA		0.5
+#define 	APPROX_EPS          1.0e-4
+#define 	APPROX_NONZDELTA    0.05
+#define 	APPROX_ZDELTA       0.00025
+#define		APPROX_ALPHA        1.0
+#define		APPROX_BETA         2.0
+#define		APPROX_GAMMA        0.5
+#define		APPROX_DELTA        0.5
 
 extern mp_obj_module_t ulab_approx_module;
 
