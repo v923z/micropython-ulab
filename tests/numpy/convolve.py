118d66c771bf8dd0d790dfc8589342bcd44e83f6
Zoltán Vörös <zvoros@gmail.com>
2020-11-25 17:23:38 +0100

    added convolve test

---
+++ tests/numpy/convolve.py
@@ -0,0 +1,18 @@
+import math
+
+try:
+    from ulab import numpy as np
+    use_ulab = True
+except ImportError:
+    import numpy as np
+    from scipy import signal
+    use_ulab = False
+
+x = np.array((1,2,3))
+y = np.array((1,10,100,1000))
+result = (np.convolve(x, y))
+ref_result = np.array([1, 12, 123, 1230, 2300, 3000],dtype=np.float)
+cmp_result = []
+for p,q in zip(list(result), list(ref_result)):
+    cmp_result.append(math.isclose(p, q, rel_tol=1e-06, abs_tol=1e-06))
+print(cmp_result)
