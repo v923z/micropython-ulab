655aa2ccd13a4187077b1334961945ab1650f99f
Phil Howard <phil@gadgetoid.com>
2021-03-23 11:59:57 +0000

    Remove debug output from code/usermod.cmake

--- code/usermod.cmake
+++ code/usermod.cmake
@@ -2,9 +2,6 @@ add_library(usermod_ulab INTERFACE)
 
 file(GLOB_RECURSE ULAB_SOURCES ${CMAKE_CURRENT_LIST_DIR}/*.c)
 
-message(WARNING ${CMAKE_CURRENT_LIST_DIR})
-message(WARNING ${ULAB_SOURCES})
-
 target_sources(usermod_ulab INTERFACE
     ${ULAB_SOURCES}
 )
