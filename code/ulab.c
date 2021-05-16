b0679e6d16d87f5acb09dee690a71a54d4c2892b
vikas-udupa <vudupa@andrew.cmu.edu>
2021-05-15 23:42:02 -0400

    implemented cho_solve function in scipy.linalg

--- code/ulab.c
+++ code/ulab.c
@@ -34,7 +34,7 @@
 #include "user/user.h"
 #include "utils/utils.h"
 
-#define ULAB_VERSION 2.7.1
+#define ULAB_VERSION 2.8.0
 #define xstr(s) str(s)
 #define str(s) #s
 #define ULAB_VERSION_STRING xstr(ULAB_VERSION) xstr(-) xstr(ULAB_MAX_DIMS) xstr(D)
