551221977797cec49038f607998b2e3f3f798e51
Zoltán Vörös <zvoros@gmail.com>
2021-11-20 11:10:06 +0100

    add complex utilities

---
+++ code/numpy/carray/carray.h
@@ -0,0 +1,18 @@
+
+/*
+ * This file is part of the micropython-ulab project,
+ *
+ * https://github.com/v923z/micropython-ulab
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 Zoltán Vörös
+*/
+
+#ifndef _CARRAY_
+#define _CARRAY_
+
+MP_DECLARE_CONST_FUN_OBJ_1(carray_real_obj);
+MP_DECLARE_CONST_FUN_OBJ_1(carray_imag_obj);
+
+#endif
