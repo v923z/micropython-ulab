77a856e84ebc09e07ba245334d5c6cc0b318535f
Zoltán Vörös <zvoros@gmail.com>
2022-01-25 18:10:06 +0100

    fix compilation problem on the rp2 port

--- code/numpy/io/io.h
+++ code/numpy/io/io.h
@@ -11,6 +11,12 @@
 #ifndef _ULAB_IO_
 #define _ULAB_IO_
 
+#if MICROPY_VFS
+#define io_open_stream mp_vfs_open
+#else
+#define io_open_stream mp_builtin_open
+#endif
+
 MP_DECLARE_CONST_FUN_OBJ_2(io_save_obj);
 MP_DECLARE_CONST_FUN_OBJ_1(io_load_obj);
 
