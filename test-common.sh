e5b8a6f3d80f24bc7b22e7e92abb828ea5e47e94
Zoltán Vörös <zvoros@gmail.com>
2022-07-27 22:44:54 +0200

    fix missing expected output issue

--- test-common.sh
+++ test-common.sh
@@ -6,14 +6,22 @@ for level1 in $(printf "%dd " $(seq 1 ${dims}))
 do
     for level2 in numpy scipy utils complex; do
         rm -f *.exp
-	if ! env MICROPY_MICROPYTHON="$micropython" ./run-tests -d tests/"$level1"/"$level2"; then
+        if [ ! -d tests/"$level1"/"$level2" ]; then
+            break;
+        fi
+        for file in tests/"$level1"/"$level2"/*.py; do
+            if [ ! -f "$file"".exp"  ]; then
+                echo "" > "$file"".exp"
+            fi
+        done
+	    if ! env MICROPY_MICROPYTHON="$micropython" ./run-tests -d tests/"$level1"/"$level2"; then
             for exp in *.exp; do
                 testbase=$(basename $exp .exp);
-                echo -e "\nFAILURE $testbase";
+                echo "\nFAILURE $testbase";
                 diff -u $testbase.exp $testbase.out;
             done
             exit 1
-	fi
+	    fi
     done
 done
 
