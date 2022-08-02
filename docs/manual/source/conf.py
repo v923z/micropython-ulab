52070daa1c5eeeb539bdab00dc66b45c1adfce06
Zoltán Vörös <zvoros@gmail.com>
2022-08-02 21:54:44 +0200

    add nonzero documentation

--- docs/manual/source/conf.py
+++ docs/manual/source/conf.py
@@ -27,7 +27,7 @@ copyright = '2019-2022, Zoltán Vörös and contributors'
 author = 'Zoltán Vörös'
 
 # The full version, including alpha/beta/rc tags
-release = '5.0.2'
+release = '5.1.0'
 
 
 # -- General configuration ---------------------------------------------------
