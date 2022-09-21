5ccfa5cdd9040c2c4219c07b005256427d31ed1c
Zoltán Vörös <zvoros@gmail.com>
2022-09-21 20:10:18 +0200

    Update build.sh
    remove tests from nanbox

--- build.sh
+++ build.sh
@@ -66,6 +66,5 @@ make -C micropython/ports/unix -j${NPROC} USER_C_MODULES="${HERE}" DEBUG=1 STRIP
 # macOS doesn't support i386 builds so only build on linux.
 if [ $PLATFORM = linux ]; then
     make -C micropython/ports/unix -j${NPROC} VARIANT=nanbox USER_C_MODULES="${HERE}" DEBUG=1 STRIP=: MICROPY_PY_FFI=0 MICROPY_PY_BTREE=0 CFLAGS_EXTRA=-DULAB_MAX_DIMS=$dims CFLAGS_EXTRA+=-DULAB_HASH=$GIT_HASH BUILD=build-nanbox-$dims PROG=micropython-nanbox-$dims
-    bash test-common.sh "${dims}" "micropython/ports/unix/micropython-$dims"
 fi
 
