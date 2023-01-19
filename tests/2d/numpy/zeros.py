315c988393ec9527364110e8a5e5dee94f7d1557
Jeff Epler <jepler@gmail.com>
2023-01-19 07:13:14 -0600

    Check that array size doesn't overflow at construction time
    Now, requesting to allocate an array that is too big gives the exception
    'array is too big', like numpy.
    
    This does depend on a gcc extension, `__builtin_mul_overflow`, present
    since at least version 5. This extension is also supported in clang.
    msvc is probably the only compiler of note that does not support it.
    
    Closes: #576

--- tests/2d/numpy/zeros.py
+++ tests/2d/numpy/zeros.py
@@ -11,3 +11,18 @@ print(np.zeros((3,3)))
 for dtype in dtypes:
     print(np.zeros((3,3), dtype=dtype))
     print(np.zeros((4,2), dtype=dtype))
+
+try:
+    np.zeros((1<<31, 1<<31))
+except ValueError:
+    print("ValueError")
+
+try:
+    np.zeros((2147483653, 2147483649))
+except ValueError:
+    print("ValueError")
+
+try:
+    np.zeros((194899806, 189294637612))
+except ValueError:
+    print("ValueError")
