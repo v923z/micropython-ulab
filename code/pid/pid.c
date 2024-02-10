a9fec6461f8c746689818fb83bb755461381343d
Zoltán Vörös <zvoros@gmail.com>
2024-02-10 18:53:46 +0100

    add some helper methods

--- code/pid/pid.c
+++ code/pid/pid.c
@@ -12,9 +12,11 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
-#include "py/obj.h"
 #include "py/runtime.h"
 #include "py/misc.h"
+#include "py/obj.h"
+#include "py/objstr.h"
+#include "py/objtuple.h"
 
 
 #include "pid.h"
@@ -23,8 +25,10 @@
 
 // methods of the PID object
 static const mp_rom_map_elem_t ulab_pid_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_bitdepth), MP_ROM_PTR(&pid_pid_bitdepth_obj) },
     { MP_ROM_QSTR(MP_QSTR_reset), MP_ROM_PTR(&pid_pid_reset_obj) },
-    // { MP_ROM_QSTR(MP_QSTR_set_buffer), MP_ROM_PTR(&pid_pid_set_buffer_obj) },
+    // { MP_ROM_QSTR(MP_QSTR_buffer), MP_ROM_PTR(&pid_pid_buffer_obj) },
+    { MP_ROM_QSTR(MP_QSTR_series), MP_ROM_PTR(&pid_pid_series_obj) },
     { MP_ROM_QSTR(MP_QSTR_step), MP_ROM_PTR(&pid_pid_step_obj) },
     { MP_ROM_QSTR(MP_QSTR_value), MP_ROM_PTR(&pid_pid_value_obj) },
 };
@@ -54,8 +58,6 @@ void pid_pid_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t ki
     (void)kind;
     pid_obj_t *self = MP_OBJ_TO_PTR(self_in);
     mp_printf(MP_PYTHON_PRINTER, "PID object at 0x%p", self);
-    mp_printf(MP_PYTHON_PRINTER, "\nin buffer at 0x%p, out buffer at 0x%p", self->in.buffer, self->out.buffer);
-    mp_printf(MP_PYTHON_PRINTER, "\n(P: %f, I: %f, D: %f)", self->P, self->I, self->D);
     mp_printf(MP_PYTHON_PRINTER, "\nvalue: %f", self->value);
     mp_printf(MP_PYTHON_PRINTER, "\nlast time: %f", self->last_time);
     mp_printf(MP_PYTHON_PRINTER, "\nsteps: %lu", self->steps);
@@ -69,6 +71,8 @@ mp_obj_t pid_pid_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw,
 
     pid_obj_t *self = m_new_obj(pid_obj_t);
     self->base.type = &ulab_pid_type;
+    self->in.series.len = 0;
+    self->out.series.len = 0;
     self->P = MICROPY_FLOAT_CONST(0.0);
     self->I = MICROPY_FLOAT_CONST(0.0);
     self->D = MICROPY_FLOAT_CONST(0.0);
@@ -79,7 +83,37 @@ mp_obj_t pid_pid_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw,
     return self;
 }
 
+mp_obj_t pid_pid_bitdepth(size_t n_args, const mp_obj_t *args) {
+    // set/get resolution of ADC/DAC and mask
+    pid_obj_t *self = MP_OBJ_TO_PTR(args[0]);
+    GET_STR_DATA_LEN(args[1], which, length);
+
+    pid_buffer_t *buffer;
+
+    if(memcmp(which, "in", 2) == 0) {
+        buffer = &(self->in.buffer);
+    } else if(memcmp(which, "out", 3) == 0) {
+        buffer = &(self->out.buffer);
+    } else {
+        mp_raise_ValueError(MP_ERROR_TEXT("series must be location 'in' or 'out'"));
+    }
+    if(n_args == 2) { // this is a getter
+        return MP_OBJ_NEW_SMALL_INT(buffer->bitdepth);
+    }
+    
+    if(n_args == 3) { // this is a setter
+        uint8_t bitdepth = (uint8_t)MP_OBJ_SMALL_INT_VALUE(args[2]);
+        buffer->bitdepth = bitdepth;
+        buffer->mask = (1 << bitdepth) - 1;
+    }
+    return mp_const_none;
+}
+
+MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(pid_pid_bitdepth_obj, 2, 3, pid_pid_bitdepth);
+
+
 mp_obj_t pid_pid_reset(mp_obj_t _self) {
+    // resets only the PID values but not the parameters
     pid_obj_t *self = MP_OBJ_TO_PTR(_self);
     self->integral = MICROPY_FLOAT_CONST(0.0);
     self->value = MICROPY_FLOAT_CONST(0.0);
@@ -90,6 +124,68 @@ mp_obj_t pid_pid_reset(mp_obj_t _self) {
 }
 
 MP_DEFINE_CONST_FUN_OBJ_1(pid_pid_reset_obj, pid_pid_reset);
+
+/*
+static mp_float_t pid_evaluate_series(pid_series_t series, mp_float_t x) {
+
+    mp_float_t dx = x - series.x0;
+    mp_float_t *coeffs = (mp_float_t *)series.coeffs;
+    mp_float_t y = *coeffs++;
+
+    for(uint8_t i = 0; i < series.len - 1; i++) {
+        y *= dx;
+        y += *coeffs++;
+    }
+    return y;
+}
+*/
+
+mp_obj_t pid_pid_series(size_t n_args, const mp_obj_t *args) {
+    // set/get coefficients and positions of Taylor series expansion 
+    pid_obj_t *self = MP_OBJ_TO_PTR(args[0]);
+    GET_STR_DATA_LEN(args[1], which, length);
+
+    pid_series_t *series;
+
+    if(memcmp(which, "in", 2) == 0) {
+        series = &(self->in.series);
+    } else if(memcmp(which, "out", 3) == 0) {
+        series = &(self->out.series);
+    } else {
+        mp_raise_ValueError(MP_ERROR_TEXT("series must be location 'in' or 'out'"));
+    }
+
+    mp_float_t *coeffs = (mp_float_t *)series->coeffs;
+
+    if(n_args == 2) { // this is a getter
+        mp_obj_t *items = m_new(mp_obj_t, series->len);
+        for(uint8_t i = 0; i < series->len; i++) {
+            items[i] = mp_obj_new_float(*coeffs++);
+        }
+        mp_obj_t tuple = mp_obj_new_tuple(series->len, items);
+        m_del(mp_obj_t, items, series->len);
+        return tuple;
+    }
+    
+    if(n_args == 3) { // this is a setter
+        m_del(mp_float_t, coeffs, series->len);
+        uint8_t len = (uint8_t)MP_OBJ_SMALL_INT_VALUE(mp_obj_len_maybe(args[2]));
+        coeffs = m_new0(mp_float_t, len);
+        series->len = len;
+        series->coeffs = coeffs;
+
+        mp_obj_iter_buf_t buf;
+        mp_obj_t item, iterable = mp_getiter(args[2], &buf);
+        while ((item = mp_iternext(iterable)) != MP_OBJ_STOP_ITERATION) {
+            *coeffs++ = (mp_float_t)mp_obj_get_float(item);
+            coeffs++;
+        }
+    }
+    return mp_const_none;
+}
+
+MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(pid_pid_series_obj, 2, 3, pid_pid_series);
+
 
 /*
 mp_obj_t pid_pid_set_buffer(mp_obj_t _self, mp_obj_t _bytes, mp_obj_t _offset) {
--- code/pid/pid.h
+++ code/pid/pid.h
@@ -17,34 +17,32 @@ extern const mp_obj_module_t ulab_pid_module;
 
 extern const mp_obj_type_t ulab_pid_type;
 
-enum PID_BUFFER_TYPE {
-    PID_UINT8 = 'B',
-    PID_INT8 = 'b',
-    PID_UINT16 = 'H',
-    PID_INT16 = 'h',
-    PID_UINT32 = 'I',
-    PID_INT32 = 'i',
-};
+// structure holding the Taylor series representation of 
+// the conversion of physical values to unitless numbers
+typedef struct _pid_series_t {
+    uint8_t len;            // order of the Taylor expansion
+    mp_float_t x0;          // the function is expanded around this point
+    mp_float_t *coeffs;     // pointer to coefficients of the Taylor series
+} pid_series_t;
 
+// structure holding the description and byte data of the input/output buffers
 typedef struct _pid_buffer_t {
-    uint8_t len;
-    uint8_t dtype;
-    uint8_t offset;
-    uint8_t *buffer;
+    uint8_t len;            // length of the input/output buffer
+    uint8_t bitdepth;       // resolution of ADC/DAC       
+    uint32_t mask;          // mask to get rid non-numerical data
+    uint8_t offset;         // offset of beginning of data relative to beginning of buffer
+    uint8_t *bytes;         // pointer to bytes of the buffer
 } pid_buffer_t;
 
-typedef struct _pid_polynomial_t {
-    uint8_t len;
-    mp_float_t *coeffs;
-} pid_polynomial_t;
-
+typedef struct _pid_data_converter_t {
+    pid_buffer_t buffer;
+    pid_series_t series;
+} pid_data_converter_t;
 
 typedef struct _pid_obj_t {
     mp_obj_base_t base;
-    pid_buffer_t in;
-    pid_buffer_t out;
-    pid_polynomial_t in_convert;
-    pid_polynomial_t out_convert;
+    pid_data_converter_t in;
+    pid_data_converter_t out;
     mp_float_t P;
     mp_float_t I;
     mp_float_t D;
@@ -57,9 +55,11 @@ typedef struct _pid_obj_t {
 mp_obj_t pid_pid_make_new(const mp_obj_type_t *, size_t , size_t , const mp_obj_t *);
 void pid_pid_print(const mp_print_t *, mp_obj_t , mp_print_kind_t );
 
-
+MP_DECLARE_CONST_FUN_OBJ_VAR_BETWEEN(pid_pid_bitdepth_obj);
+MP_DECLARE_CONST_FUN_OBJ_3(pid_pid_buffer_obj);
+MP_DECLARE_CONST_FUN_OBJ_3(pid_pid_convert_obj);
 MP_DECLARE_CONST_FUN_OBJ_1(pid_pid_reset_obj);
-MP_DECLARE_CONST_FUN_OBJ_3(pid_pid_set_buffer_obj);
+MP_DECLARE_CONST_FUN_OBJ_VAR_BETWEEN(pid_pid_series_obj);
 MP_DECLARE_CONST_FUN_OBJ_1(pid_pid_step_obj);
 MP_DECLARE_CONST_FUN_OBJ_1(pid_pid_value_obj);
 
