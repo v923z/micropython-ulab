7e5b67d939ea6287702fa268bf9ef78ebf30991d
Harald Milz <hm@seneca.muc.de>
2024-12-14 15:00:55 +0100

    improve tests using math.isclose()

--- tests/2d/scipy/integrate.py
+++ tests/2d/scipy/integrate.py
@@ -14,8 +14,20 @@ except Exception as e:
 f = lambda x: x * sin(x) * exp(x)
 a=1
 b=2
-functions = ( scipy.integrate.quad, scipy.integrate.romberg, scipy.integrate.simpson, scipy.integrate.quadgk )
-for function in functions:
-	    print (function(f, a, b))
 
+(res, err) = scipy.integrate.quad(f, a, b)
+if isclose (res, 7.11263821415851) and isclose (err, 5.438231077315757e-14):
+	print (res, err)
+		
+res = scipy.integrate.romberg(f, a, b)
+if isclose (res, 7.112638214158507):
+	print (res)
+
+res = scipy.integrate.simpson(f, a, b)
+if isclose (res, 7.112638214158494):
+	print (res)
+
+(res, err) = scipy.integrate.quadgk(f, a, b)
+if isclose (res, 7.112638214158507) and isclose (err, 7.686723611780195e-14):
+		print (res, err)
 
