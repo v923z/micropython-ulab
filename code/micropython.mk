73ed8cc11f57c5c7605d97eaf5303687a2d21106
Harald Milz <149242962+h-milz@users.noreply.github.com>
2024-12-15 18:49:08 +0100

    Add scipy integration (#699)
    * adding scipy integrate, initial check-in
    
    * compile unix double-precision, select integrations algos
    
    * bumping ulab version number to 6.7.0
    
    * adding documentation
    
    * documentation fix
    
    * documentation fix
    
    * documentation fix
    
    * rewritten in some places
    
    * complex number error handling
    
    * added test cases
    
    * resolved importing scipy.integrate
    
    * resolved importing scipy.integrate #2
    
    * build integrate only when we have MICROPY_FLOAT_IMPL_DOUBLE
    
    * reverting commit a4c0c0b
    
    * re-pushing failed commit
    
    * Revert "re-pushing failed commit"
    
    This reverts commit a10e89fe14ce17306bb35e629509c01b1b6f8094.
    
    * improve tests using math.isclose()
    
    * enabled fp32 builds
    
    * removed conditional includes
    
    * adapted to new function names, corrected importing
    
    * function names similar to in CPython scipy.integrate, some minor corrections
    
    * major rewrite representing the name changes, mapping to CPython scipy.integrate, more background info

--- code/micropython.mk
+++ code/micropython.mk
@@ -2,6 +2,7 @@
 USERMODULES_DIR := $(USERMOD_DIR)
 
 # Add all C files to SRC_USERMOD.
+SRC_USERMOD += $(USERMODULES_DIR)/scipy/integrate/integrate.c
 SRC_USERMOD += $(USERMODULES_DIR)/scipy/linalg/linalg.c
 SRC_USERMOD += $(USERMODULES_DIR)/scipy/optimize/optimize.c
 SRC_USERMOD += $(USERMODULES_DIR)/scipy/signal/signal.c
